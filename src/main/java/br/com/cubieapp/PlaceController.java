d6dd74d9eff98f6d6be8e64cdffd3316cdb1aaa1
Marcos Arruda <marcos.f.arruda@gmail.com>
2014-01-07 17:35:03 -0200

    Cleaning and start adding carousel.

--- src/main/java/br/com/cubieapp/PlaceController.java
+++ src/main/java/br/com/cubieapp/PlaceController.java
@@ -1,6 +1,5 @@
 package br.com.cubieapp;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping("/a")
-public class AutomationController {
+public class PlaceController {
 	@RequestMapping(value = "/sala", method = RequestMethod.GET)
 	public String sala(Model model)
 	{
